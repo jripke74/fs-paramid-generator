5b47b5cfe87469c9d15135e9c66c5a9d387dbb3b
Jeff Ripke <jripke74@gmail.com>
2025-06-24 10:42:08 -0700

    change character to patternChar

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function pyramid(character, numOfRows, isInverted) {
+function pyramid(patternChar, numOfRows, isInverted) {
   let level = "";
   if (isInverted === false) {
     for (let i = 1; i <= numOfRows; i++) {
-      level += " ".repeat(numOfRows - i) + character.repeat(2 * i - 1) + "\n";
+      level += " ".repeat(numOfRows - i) + patternChar.repeat(2 * i - 1) + "\n";
     }
   } else {
-    for (let i = rows; i >= 1; i--) {
-      level += " ".repeat(numOfRows - i) + character.repeat(2 * i - 1) + "\n";
+    for (let i = numOfRows; i >= 1; i--) {
+      level += " ".repeat(numOfRows - i) + patternChar.repeat(2 * i - 1) + "\n";
     }
   }
   return "\n" + level;
 }
 
 console.log(pyramid("o", 4, false));
+
+//    o
+//   ooo
+//  ooooo
+// ooooooo
+
+console.log(pyramid("o", 4, true));
+
+// ooooooo
+//  ooooo
+//   ooo
+//    o
